6416cbe1a0475b9013b032028e2b38eccaa35e3e
smillwvy <171736343+smillwvy@users.noreply.github.com>
2025-12-05 15:36:53 +0000

    Refactor App structure to use StatefulWidget and implement routing with Cart integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:sandwich_shop/views/order_screen.dart';
+import 'package:sandwich_shop/models/cart.dart';
 import 'package:sandwich_shop/views/about_screen.dart';
+import 'package:sandwich_shop/views/app_routes.dart';
+import 'package:sandwich_shop/views/order_screen.dart';
+import 'package:sandwich_shop/views/cart_screen.dart';
+import 'package:sandwich_shop/views/profile_screen.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final Cart _cart = Cart();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sandwich Shop App',
-      home: const OrderScreen(maxQuantity: 5),
+      initialRoute: AppRoutes.order,
       routes: {
-        '/about': (context) => const AboutScreen(),
+        AppRoutes.order: (context) => OrderScreen(
+              cart: _cart,
+              maxQuantity: 5,
+            ),
+        AppRoutes.cart: (context) => CartScreen(cart: _cart),
+        AppRoutes.profile: (context) => const ProfileScreen(),
+        AppRoutes.about: (context) => const AboutScreen(),
       },
     );
   }
